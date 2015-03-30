76768c80ab3ad00307dabd85f1684842c525865b
danyRafina <danyrafina@gmail.com>
2015-03-30 17:44:45 +0200

    Commit 2 - Création des packages , classes associées et interface

---
+++ src/fr/noixcoop/nuceus/presentation/ControleurNuceus.java
@@ -0,0 +1,89 @@
+package fr.noixcoop.nuceus.presentation;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.noixcoop.nuceus.logiqueMetier.MetierVarietes;
+import fr.noixcoop.nuceus.metier.Variete;
+
+/**
+ * Servlet implementation class ControleurNuceus
+ */
+public class ControleurNuceus extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private MetierVarietes metierVarietes ;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ControleurNuceus() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init() throws ServletException {
+		metierVarietes = new MetierVarietes();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		if(action == null)
+			action ="visualiser";
+
+		if(action.equals("visualiser") || action.equals("annulerAjout")) {
+			request.setAttribute("varietes",metierVarietes.consulter());
+			getServletContext().getRequestDispatcher("/WEB-INF/vues/vueListe.jsp").forward(request,response);
+		}
+		else if(action.equals("renseignerAjout")) 
+			getServletContext().getRequestDispatcher("/WEB-INF/vues/vueFormulaireAjout.jsp").forward(request,response);
+
+		else if (action.equals("ajouter")) {
+			String libelle = request.getParameter("libelle");
+			String aoc = request.getParameter("aoc");
+			boolean aocObtenu = false;
+			if(aoc == null ){
+				aoc = "non";
+				aocObtenu = false;
+			}
+			else {
+				aoc="oui";
+				aocObtenu =true;
+			}
+			boolean ajouterOk = metierVarietes.ajouter(new Variete(libelle,aocObtenu));
+			System.out.println(ajouterOk);
+			if(ajouterOk) {
+				request.setAttribute("libelle",libelle);
+				request.setAttribute("aoc",aoc);
+				System.out.println("ADA"+metierVarietes.consulter().toString());
+				getServletContext().getRequestDispatcher("/WEB-INF/vues/vueResultatAjout.jsp").forward(request,response);
+			}
+			else 
+				getServletContext().getRequestDispatcher("/WEB-INF/vues/vueResultatAjout.jsp").forward(request,response);
+
+		}
+		else if(action.equals("Supprimer")) {
+			String libelle = request.getParameter("variete");
+			boolean sup = metierVarietes.supprimer(libelle);
+			if(sup){
+				request.setAttribute("varietes",metierVarietes.consulter());
+				getServletContext().getRequestDispatcher("/WEB-INF/vues/vueListe.jsp").forward(request,response);
+			}
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/fr/noixcoop/nuceus/technique/ConnexionBD.java
@@ -0,0 +1,24 @@
+package fr.noixcoop.nuceus.technique;
+import java.sql.* ;
+import com.mysql.jdbc.Connection;
+
+public class ConnexionBD {
+	private static String dbURL ="jdbc:mysql://localhost:3306/noixcoop" , user ="root" , password ="azerty";
+	private static Connection connexion = null;
+	private ConnexionBD() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance() ;
+			connexion = (Connection) DriverManager.getConnection(dbURL,user,password);
+		}
+		catch(Exception e){
+			System.out.println("Erreur BD") ;
+		}
+	}
+
+	public static Connection getConnexion(){
+		if(connexion == null) 
+			new ConnexionBD();
+		return connexion;
+	}
+}
+
